c1d22897f2dfcbb1500f0acbe8ce1be44de5c9e1
Ville Kivelä <vkivela@hotmail.com>
2025-02-24 22:31:08 +0200

    Bump version to 1.0.7

--- bin/tp.js
+++ bin/tp.js
@@ -13,7 +13,7 @@ const timeTracker = new TimeTracker();
 program
 	.name('tp')
 	.description('TimePort - Bridge between Jira and Harvest time tracking')
-	.version('1.0.6')
+	.version('1.0.7')
 	.action(async () => {
 		try {
 			await timeTracker.run();
